3484c4f2c6eadf9ea136cae010883eb2fe7201ee
Federico <39010131+FedericoParente@users.noreply.github.com>
2025-06-05 23:50:29 +0200

    feat(frontend): add dashboard homepage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={createTheme()}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
